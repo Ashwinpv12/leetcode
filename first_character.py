cc4628c8bef7f490d0f9a70258659c27378b6ac6
Ashwin p v <117075074+Ashwinpv12@users.noreply.github.com>
2026-09-03 23:55:06 +0530

    Add files via upload

---
+++ first_character.py
@@ -0,0 +1,10 @@
+#first unique character in a string
+from collections import Counter
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        counts = Counter(s)
+        for i, c in enumerate(s):
+            if counts[c] == 1:
+                return i
+
+        return -1        
